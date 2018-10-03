50661f0c6cdbfd069080591b03c44bdbc0d930e4
Donna Cadiente <djanecad@hotmail.com>
2018-10-03 19:08:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const alphabet = [65, 66, 67]
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
       index = 0;
     }
@@ -13,6 +12,8 @@ function onKeyDownHandler(e) {
     index = 0;
   }
 }
+
+let index = 0
 
 function init() {
   document.body.addEventListener('keydown',function(event) {
